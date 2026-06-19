a5e496ad3488a76c64c250b3de84be606a700325
jlijano <33651602+jlijano@users.noreply.github.com>
2026-06-19 08:18:45 +0800

    Remove structured process JS compatibility entry

--- assets/js/process-interaction.js
+++
@@ -1,6 +0,0 @@
-(function () {
-  const script = document.createElement("script");
-  script.defer = true;
-  script.src = "/assets/process-interaction.js";
-  (document.currentScript || document.head).after(script);
-})();

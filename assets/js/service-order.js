4823b1bf3e6707101cb4e814b487f5c6aa212227
jlijano <33651602+jlijano@users.noreply.github.com>
2026-06-19 08:18:57 +0800

    Remove structured service-order JS compatibility entry

--- assets/js/service-order.js
+++
@@ -1,6 +0,0 @@
-(function () {
-  const script = document.createElement("script");
-  script.defer = true;
-  script.src = "/assets/service-order.js";
-  (document.currentScript || document.head).after(script);
-})();

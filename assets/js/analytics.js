eba7e2b614b9aa4d8ea5108c3719dc97e22080a8
jlijano <33651602+jlijano@users.noreply.github.com>
2026-06-19 08:18:31 +0800

    Remove structured analytics JS compatibility entry

--- assets/js/analytics.js
+++
@@ -1,6 +0,0 @@
-(function () {
-  const script = document.createElement("script");
-  script.defer = true;
-  script.src = "/assets/analytics.js";
-  (document.currentScript || document.head).after(script);
-})();

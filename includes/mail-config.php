6b053f5dc19110524e7fb54bf8cae86572aacb65
jlijano <33651602+jlijano@users.noreply.github.com>
2026-07-02 23:22:22 +0800

    Restore Hostinger-compatible portal mail sender

---
+++ includes/mail-config.php
@@ -0,0 +1,7 @@
+<?php
+declare(strict_types=1);
+
+return [
+    'portal_base_url' => 'https://sandbox.oligarchyservices.com',
+    'portal_mail_from' => 'no-reply@sandbox.oligarchyservices.com',
+];

cb321675bd99e79c6f0b91f1c11aa02aa710d8a3
jlijano <33651602+jlijano@users.noreply.github.com>
2026-07-02 23:41:14 +0800

    Remove committed server-specific mail config

--- includes/mail-config.php
+++
@@ -1,7 +0,0 @@
-<?php
-declare(strict_types=1);
-
-return [
-    'portal_base_url' => 'https://sandbox.oligarchyservices.com',
-    'portal_mail_from' => 'no-reply@sandbox.oligarchyservices.com',
-];
